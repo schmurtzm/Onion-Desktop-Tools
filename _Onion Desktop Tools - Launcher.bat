3f12056fb8055a998caa758bd20a563d72d88000
Schmurtz <Schmurtz@gmail.com>
2024-01-05 12:29:25 +0100

    Removing launcher binaries and "Bat 2 Exe"
    Fix antivirus false positive detections
    Todo : A compiled launcher (C# or C++ for example) with a dedicated icon.
    For now the launchers are simple .bat files.

---
+++ _Onion Desktop Tools - Launcher.bat
@@ -0,0 +1,11 @@
+FSUTIL DIRTY query %SystemDrive% >NUL || (
+    PowerShell "Start-Process -FilePath cmd.exe -Args '/C CHDIR /D %CD% & "%0"' -Verb RunAs"
+    EXIT
+)
+
+@echo off
+cd /d %~dp0
+md backups 2> NUL
+md downloads\ODT_updates 2> NUL
+copy ODT_update.ps1 ODT_update_temporary.ps1 /Y >NUL
+powershell -ExecutionPolicy Bypass -WindowStyle hidden -File ODT_update_temporary.ps1
